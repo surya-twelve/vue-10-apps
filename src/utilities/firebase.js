8f73a1a07bbc68800e86f796433399f28dbda64d
dodiwkwk <arifaldo0@gmail.com>
2021-01-26 15:41:06 +0700

    api key firebase removed

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -3,12 +3,12 @@ import "firebase/auth";
 import "firebase/database";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAwTRMW0PMDxqutB2Fnqqbxf5r8vrw8BB8",
+  apiKey: "",
   authDomain: "vue-full-course-6db5c.firebaseapp.com",
   projectId: "vue-full-course-6db5c",
   storageBucket: "vue-full-course-6db5c.appspot.com",
   messagingSenderId: "22088346735",
-  appId: "1:22088346735:web:60dccb50292e3f241a319a",
+  appId: "",
 };
 
 // Initialize Firebase
